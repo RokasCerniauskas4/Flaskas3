3826eb2587a7d0b9ab27b27db77b7a1b2a50fbd5
Rokas Černiauskas <Rokascerniauskas4@gmail.com>
2022-11-22 20:01:28 +0200

    Idedu vfailus

--- forms.py
+++ forms.py
@@ -1,7 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
+from wtforms_alchemy.fields import QuerySelectField
+
 
 
 class AddClientForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
+
+class CreateProductForm(FlaskForm):
+    code = StringField('code', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired()])
+
+
--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from forms import AddClientForm
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
+
+
 db = SQLAlchemy()
 app = Flask(__name__)
 migrate = Migrate(app, db)
@@ -16,10 +19,28 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_database'
 db.init_app(app)
 
 class Client(db.Model):
+    __tablename__='client'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
     address = db.Column(db.String)
 
+class Product(db.Model):
+    __tablename__ = 'product'
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String)
+    stock = db.relationship('Stock', back_populates='product')
+
+class Stock(db.Model):
+    __tablename__ = 'stock'
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    product = db.relationship('Product', back_populates='stock')
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
 
 with app.app_context():
     db.create_all()
@@ -49,6 +70,23 @@ def show_clients():
     #clients = db.session.execute(db.select(Client)).scalars()
 
     return render_template('clients.html', data=clients)
+@app.route('/add_product', methods=['GET','POST'])
+def add_product():
+    form = ProductForm()
+    if request.method == 'POST':
+        product = Product(code=form.code.data, name=form.name.data)
+        db.session.add(product)
+        db.session.commit()
+        return redirect('add_product.html')
+    return render_template('add_product.html', form=form)
+
+@app.route('/add_stock', methods=['GET','POST'])
+def add_stock():
+    return render_template('add_stock.html')
+
+@app.route('/show_stock')
+def show_stock():
+    return render_template('show_stock.html')
 
 
 if __name__ == '__main__':
